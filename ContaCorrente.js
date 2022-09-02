e82382251fd5d626adb3003ae3575bf632ff0fd7
Luiz Barcelos <luizaugusto1886@gmail.com>
2022-09-01 22:45:47 -0300

    01.09

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,10 +1,30 @@
+import { Cliente } from "./Cliente.js";
 export class ContaCorrente {
+    static numeroDeContas = 0;
     agencia;
-    cliente;
+    _cliente;
 
- 
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
+
     _saldo = 0;
 
+    get saldo() {
+        return this._saldo;
+    }
+
+    constructor(cliente, agencia) {
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
 
     sacar(valor) {
         if (this._saldo >= valor) {
@@ -20,7 +40,7 @@ export class ContaCorrente {
         this._saldo += valor;
     }
 
-    transferir(valor, conta){
+    transferir(valor, conta) {
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
     }
--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Robert";
-cliente1.cpf = 12332132117;
+// const cliente1 = new Cliente();
+// cliente1.nome = "Robert";
+// cliente1.cpf = 12332132117;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Mick";
-cliente2.cpf = 12332132118;
+// const cliente2 = new Cliente();
+// cliente2.nome = "Mick";
+// cliente2.cpf = 12332132118;
 
-const ContaCorrenteRobert = new ContaCorrente();
-ContaCorrenteRobert.agencia = 1001;
-ContaCorrenteRobert.cliente = cliente1;
-ContaCorrenteRobert.depositar(1000);
+// const ContaCorrenteRobert = new ContaCorrente();
+// ContaCorrenteRobert.agencia = 1001;
+// ContaCorrenteRobert.cliente = cliente1;
+// ContaCorrenteRobert.depositar(1000);
 
-const ContaCorrenteMick = new ContaCorrente();
-ContaCorrenteMick.agencia = 1001;
-ContaCorrenteMick.cliente = cliente2;
+// const ContaCorrenteMick = new ContaCorrente();
+// ContaCorrenteMick.agencia = 1001;
+// ContaCorrenteMick.cliente = cliente2;
 
-const contaX = new ContaCorrente();
-contaX.cliente = new Cliente();
-contaX.cliente.nome = "Humberto G";
-contaX.cliente.cpf = 123456456;
-contaX.agencia = 1002;
+// const contaX = new ContaCorrente();
+// contaX.cliente = new Cliente();
+// contaX.cliente.nome = "Humberto G";
+// contaX.cliente.cpf = 123456456;
+// contaX.agencia = 1002;
 
-ContaCorrenteRobert.transferir(500, ContaCorrenteMick);
+const clienteX = new Cliente("Renato R", 12332143212);
+const contaX = new ContaCorrente(clienteX, 1001);
+
+const clienteY = new Cliente("Rita L", 98765456722);
+const contaY = new ContaCorrente(clienteY, 1003);
+
+// ContaCorrenteRobert.transferir(500, ContaCorrenteMick);
 
 
-console.log(ContaCorrenteRobert);
-console.log(ContaCorrenteMick);
-console.log(contaX);
+// console.log(ContaCorrenteRobert);
+// console.log(ContaCorrenteMick);
+
+
+console.log(ContaCorrente.numeroDeContas);
+
 
 
